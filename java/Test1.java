254c43f944299811ff4cfb810e29fee8b182a16e
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-18 17:51:47 +0300

    refactor

--- java/Test1.java
+++ java/Test1.java
@@ -4,44 +4,28 @@ import static java.lang.Integer.parseInt;
 
 public class Test1{
 
+    static int calculateFactorial(int n){
+        int r = 1;
+        for (int i = 1; i <= n; i++){
+            r = r * i;
+        }
+        return r;
+    }
+
     public static void main(String[] args){
 
-        String s = "10";
+        String string = "10";
 
-        int l = parseInt(s);
-
-        for (int i = 0; i < 100; i++) {
-            if (true) {
-                l = 10 + s;
-                System.out.println("Qwe");
-                if (true) {
-                    l = 10 + s;
-                    System.out.println("Qwe");
-                }
-            }
-            if (true) {
-                l = 10 + s;
-                System.out.println("Qwe");
-            }
-        }
+        int l = parseInt(string);
 
         int x = calculateFactorial(l);
+
         if (x > 10) {
-            while (true) {
-                while (x < 10000) {
-                    System.out.println("Test 2");
-                }
-                System.out.println("Test 2");
-            }
             System.out.println(x + " is bigger than 10");
         }
 
         if (x <= 10) {
-            if (x <= 10) {
-                System.out.println(x + " is equal or less than 10");
-            }
-            System.out.println(x + " is equal or less than 10");
-            if (x <= 10) {
+            for (int i = 0; i < 10; i++) {
                 System.out.println(x + " is equal or less than 10");
             }
         }
